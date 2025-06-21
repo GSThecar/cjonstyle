687a3b1fe45440c74ba5267fb78378523bfb4b59
GSThecar <thecar3774@gmail.com>
2025-06-21 18:11:08 +0900

    feature/Add ProductUseCase

---
+++ cjonstyle/Sources/ProductFetchError.swift
@@ -0,0 +1,12 @@
+//
+//  ProductFetchError.swift
+//  cjonstyle
+//
+//  Created by 이덕화 on 2025/06/21.
+//
+
+import Foundation
+
+enum ProductFetchError: Error {
+    case notExistFile
+}
---
+++ cjonstyle/Sources/ProductUseCase.swift
@@ -0,0 +1,46 @@
+//
+//  ProductUseCase.swift
+//  cjonstyle
+//
+//  Created by 이덕화 on 2025/06/21.
+//
+
+import Foundation
+
+import RxSwift
+
+final class ProductUseCase{
+    private let productDTOs: BehaviorSubject<[ProductDTO]> = BehaviorSubject(value: [])
+
+    func fetch() async throws {
+        guard
+            let url = Bundle.main.url(forResource: "products",
+                                  withExtension: "json")
+        else {
+            throw ProductFetchError.notExistFile
+        }
+
+        let data = try Data(contentsOf: url)
+        let jsonParser = JSONDecoder()
+        let decoded = try jsonParser.decode([ProductDTO].self, from: data)
+        productDTOs.onNext(decoded)
+    }
+
+    func observableListMetadatas() -> Observable<[ListMetadata]> {
+        productDTOs.map { dtos -> [ListMetadata] in
+            dtos.compactMap { dto -> ListMetadata? in
+                guard
+                    let imagePath = dto.image?.components(separatedBy: .whitespacesAndNewlines).joined(),
+                    let thumbnailURL = URL(string: imagePath)
+                else { return nil }
+
+                let name = dto.name ?? "-"
+                let price = dto.price ?? 0
+
+                return ListMetadata(thumbnailURL: thumbnailURL,
+                             name: name,
+                             price: String(price))
+            }
+        }
+    }
+}
