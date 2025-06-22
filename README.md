627f80c65130b126d479af5eba38fe60065202c1
GSThecar <thecar3774@gmail.com>
2025-06-22 16:09:38 +0900

    결과물 확인을 위한 readme내용 보완

--- README.md
+++ README.md
@@ -27,15 +27,18 @@ source ~/.zshrc
 open cjonstyle.xcodeproj
 ```
 
-```
-# Swift Package Manager
-"https://github.com/onevcat/Kingfisher.git"
-"https://github.com/devxoul/Then.git",
-"https://github.com/ReactiveX/RxSwift.git"
-"https://github.com/RxSwiftCommunity/RxDataSources.git"
-"https://github.com/SnapKit/SnapKit.git"
+```bash
+# Swift Package Manager (Please, manually set)
+"https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "8.3.0")
+"https://github.com/devxoul/Then.git", .upToNextMajor(from: "3.0.0")
+"https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.9.0") - RxCocoa
+"https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")
+"https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.0")
 ```
 
+```bash
+# Build it with an iphone simulator, run it, and you can see the results.
+```
 
 ## Structure
 
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -2,11 +2,11 @@ import ProjectDescription
 
 let dependencies = Dependencies(
     swiftPackageManager: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.10.0")),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "8.3.0")),
         .package(url: "https://github.com/devxoul/Then.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.6.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.9.0")),
         .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0"))
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.0"))
     ],
     platforms: [.iOS]
 )
